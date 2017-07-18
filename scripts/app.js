c98126c2a436edb955a848663e97d22c46264b07
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-07-18 13:52:03 -0300

    Declarando varias variaveis

---
+++ scripts/app.js
@@ -0,0 +1,33 @@
+// Constantes
+
+const myConst = 5;
+const myConsts = [1, 2, 3];
+myConsts.push(4); // Funciona
+myConsts = [1, 2, 3, 4]; // Não funciona
+
+// Let
+
+for(var i = 0; i < 5; i++){
+}
+console.log(i); // Funciona
+
+for(let i = 0; i < 5; i++){
+}
+console.log(i) // Não funciona
+
+var a = 2;
+{
+    let a = 3;
+    console.log(a); // Imprimi 3
+}
+console.log(a); // Imprimi 2
+
+// Declarando varias variaveis
+
+var [a, b, c] = [1, 2, 3];
+
+var a = 1;
+var b = 2;
+var c = 3;
+
+
