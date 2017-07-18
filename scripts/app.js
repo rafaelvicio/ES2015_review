ccc4957976d94352a40854b61c0093fd1f74a51e
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-07-18 15:55:11 -0300

    WeakMaps

--- scripts/app.js
+++ scripts/app.js
@@ -118,3 +118,12 @@ myObj.set( a, 5 );
 myObj.set( b, 10);
 
 console.log(myObj.get(a), myObj.get(b));
+
+// WeakMaps
+
+var myObj = new WeakMap();
+var a = { value: 1};
+var b = { value: 2};
+
+myObj.set(a, 5);
+myObj.set(b, 10);
