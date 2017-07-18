ddf78b004af481c665e7c9a93c385d4b3b6e7aba
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-07-18 15:53:55 -0300

    Maps

--- scripts/app.js
+++ scripts/app.js
@@ -108,4 +108,13 @@ var obj = {
     }
 }
 
+// Maps
 
+var myObj = new Map();
+var a = { value: 1 };
+var b = { value: 2 };
+
+myObj.set( a, 5 );
+myObj.set( b, 10);
+
+console.log(myObj.get(a), myObj.get(b));
